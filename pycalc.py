ae66a37131941153274fa3ace1351d83a8b19d5b
pAgrayB <ptgraybeal5471@gmail.com>
2023-12-22 00:04:11 -0500

    Create controller class

--- pycalc.py
+++ pycalc.py
@@ -3,6 +3,7 @@
 """ PyCalc is a simple calculator build with python and PyQt."""
 
 import sys
+from functools import partial
 
 from PyQt6.QtCore import Qt
 from PyQt6.QtWidgets import (
@@ -15,6 +16,7 @@ from PyQt6.QtWidgets import (
     QWidget,
 )
 
+ERROR_MSG = "ERROR"
 WINDOW_SIZE = 235
 DISPLAY_HEIGHT = 35
 BUTTON_SIZE = 40
@@ -68,6 +70,42 @@ class PyCalcWindow(QMainWindow):
     def clearDisplay(self):
         """Clear the display"""
         self.setDisplayText("")
+
+    def evaluateExpression(expression):
+        """Evaluate an expression (Model)"""
+        try:
+            result = str(eval(expression, {}, {}))
+        except Exception:
+            result = ERROR_MSG
+        return result
+
+class PyCalc:
+    """PyCalc's controller class."""
+
+    def __init__(self, model, view):
+        self._evaluate = model
+        self._view = view
+        self._connectSignalsAndSlots()
+
+    def _calculateResult(self):
+        result = self._evaluate(expression=self._view.displayText())
+        self._view.setDislayText(result)
+
+    def _buildExpression(self, subExpression):
+        if self._view.displayText() == ERROR_MSG:
+            self._view.clearDisplay()
+        expression = self._view.displayText + subExpression
+        self._view.setDislayText(expression)
+
+    def _connectSignalsAndSlots(self):
+        for keySymbol, button in self._view.buttonMap.items():
+            if keySymbol not in {"=", "C"}:
+                button.clicked.connect(
+                    partial(self._buildExpression, keySymbol)
+                )
+        self._view.buttonMap["="].clicked.connect(self._calculateResult)
+        self._view.display.returnPressed.connect(self._calculateResult)
+        self._view.buttonMap["C"].connect(self._view.clearDisplay)
 
 def main():
     """PyCalc's main function"""
